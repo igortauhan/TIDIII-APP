b9c3691ac7eef86fa1e5409b8277349aa7d74346
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-05-28 11:51:56 -0300

    Add currentSpentValueMonth in EnergyDataService

--- lib/services/energy_data_service.dart
+++ lib/services/energy_data_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -5,6 +6,9 @@ import 'package:tidiii/models/energy_data.dart';
 
 class EnergyDataService {
   final String _urlFindLastRecord = "http://192.168.1.2:8080/energy/last";
+
+  String _urlGetCurrentSpentValue =
+      "http://192.168.1.2:8080/storaged/current-spent";
 
   Future<EnergyData> findAll() async {
     final response =
@@ -15,5 +19,21 @@ class EnergyDataService {
     }
 
     return EnergyData.fromJson(jsonDecode(response.body));
+  }
+
+  Future<int> getCurrentSpentValueMonth(int value) async {
+    _urlGetCurrentSpentValue += "?value=$value&hours=24&days=30";
+
+    final response =
+        await http.get(Uri.parse(_urlGetCurrentSpentValue));
+
+    if (response.statusCode !=200) {
+      throw Exception("Error");
+    }
+
+    value =  jsonDecode(response.body);
+
+    debugPrint(value.toString());
+    return value ~/ 100000;
   }
 }
