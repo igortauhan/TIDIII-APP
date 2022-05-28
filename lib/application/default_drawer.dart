0d3485faf4b90b5b84d0898c0082e0d67caab1e2
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-05-28 10:03:17 -0300

    Add Drawer

---
+++ lib/application/default_drawer.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../utils/prepare_text.dart';
+
+class DefaultDrawer extends StatefulWidget {
+  const DefaultDrawer({Key? key}) : super(key: key);
+
+  @override
+  State<DefaultDrawer> createState() => _DefaultDrawerState();
+}
+
+class _DefaultDrawerState extends State<DefaultDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Colors.deepPurple,
+      child: ListView(
+        children: [
+          SizedBox(
+            height: 80.0,
+            child: DrawerHeader(
+              child: PrepareText().prepareDefaultText('Menu'),
+            ),
+          ),
+          ListTile(
+            title: PrepareText().prepareDefaultText('Calcular valor mensal'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
