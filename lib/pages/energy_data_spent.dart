69beac6cafe37896430179c78528893e713ea0d6
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-05-29 15:06:01 -0300

    Update EnergySpent Text

--- lib/pages/energy_data_spent.dart
+++ lib/pages/energy_data_spent.dart
@@ -70,7 +70,9 @@ class _EnergyDataSpentState extends State<EnergyDataSpent> {
                 "O valor é calculado baseado no uso do produto "
                 "24 horas por dia ininterruptamente.\n\n"
                 "O valor mensal é calculado com 30 dias corridos.\n\n"
-                "O valor semanal é calculado com 7 dias corridos.");
+                "O valor semanal é calculado com 7 dias corridos.\n\n"
+                "O valor é aproximado e não é levado em conta as taxas "
+                "das bandeiras.");
           },
           icon: const Icon(Icons.help),
         ),
